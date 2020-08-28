8d6f3d51e2c067eb209c968709423e824ff09f6b
ChangGyu-Ryu <59715904+ChangGyu-Ryu@users.noreply.github.com>
2020-08-28 14:44:04 +0900

    # 후기게시판, 자유게시판 isdel update 대신 delete로 수정

--- DongHang_ComeFunny/src/main/java/com/DongHang_ComeFunny/www/model/service/board/ReviewBoardServiceImpl.java
+++ DongHang_ComeFunny/src/main/java/com/DongHang_ComeFunny/www/model/service/board/ReviewBoardServiceImpl.java
@@ -140,9 +140,9 @@ public class ReviewBoardServiceImpl implements ReviewBoardService {
 		FileUtil fileUtil = new FileUtil();
 		// 3. 파일 저장경로에 있는 파일 삭제 처리
 		fileUtil.deleteFile(fileData.get(("RFSAVEPATH")));
-//		int res = reviewBoardDao.deleteFile(rfno); // 파일완전 삭제
 		// 4. isdel 컬럼값 0 -> 1로 변경
-		int res = reviewBoardDao.updateFileIsDel(rfNo); // isdel 0을 1로 변경
+//		int res = reviewBoardDao.updateFileIsDel(rfNo); // isdel 0을 1로 변경
+		int res = reviewBoardDao.deleteFile(rfNo); // 완전삭제
 		return res;
 	}
 	
@@ -199,8 +199,8 @@ public class ReviewBoardServiceImpl implements ReviewBoardService {
 	@Override
 	public int deleteReviewBoard(int rbNo) {
 		// 1. 게시글 번호로 해당 게시글에 업로드된 파일 삭제
-		reviewBoardDao.deleteReviewFileByRbno(rbNo);
-		// 2. 게시글 번호로 해당 게시글 삭제
+//		reviewBoardDao.deleteReviewFileByRbno(rbNo);
+		// 2. 게시글 번호로 해당 게시글 완전 삭제
 		int res = reviewBoardDao.deleteReviewBoard(rbNo);
 		// 성공 : 1, 실패 : 0
 		return res;
@@ -261,7 +261,9 @@ public class ReviewBoardServiceImpl implements ReviewBoardService {
 	@Override
 	public int deleteReviewComment(ReviewComment reviewComment) {
 		// 1. 댓글 삭제 시, isdel=0을 1로 변경
-		return reviewBoardDao.updateReviewCommentIsDEl(reviewComment);
+//		return reviewBoardDao.updateReviewCommentIsDEl(reviewComment);
+		// 2. 댓글 완전 삭제
+		return reviewBoardDao.deleteReviewComment(reviewComment);
 	}
 	
 	/**
